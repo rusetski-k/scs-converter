9487bbb3de103d61269f934541b3bdff7c95b193
Kirill Rusetski <rusetski.k@gmail.com>
2012-11-12 13:15:49 +0300

    moar tests; added url processing

---
+++ converter.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+from articles.components.scs.parser import *;
+import os
+import codecs
+from glob import glob
+
+ctr = 0
+triples = []
+arc_types = {"=>" : "sc-arc-common",
+             "<=" : "sc-arc-common",
+             "->": "sc-arc-main",
+             "<-": "sc-arc-main",
+             "=": "sc-edge" }
+
+pair_idtfs = {}
+
+def mk_arc_id():
+    global ctr
+    ctr += 1
+    return "pair%d" % ctr
+
+def mk_arc(connector):
+    global ctr
+    ctr += 1
+    return "pair%d/%s" % (ctr, arc_types[connector])
+
+def sgroup(item):
+    if isinstance(item.subject, SimpleIdentifierGroup):
+        if isinstance(item.object, ParseResults):
+            s = str(item.subject)
+            p = str(item.predicate)
+            for i in item.object:
+                
+                if isinstance(i.idtf, (ContentGroup, SetGroup, UrlGroup)):
+                    continue
+                
+                a_id = mk_arc_id()
+                a = "%s/%s" %(a_id, arc_types[p])
+                triples.append((s,a,i.idtf))
+                
+                if isinstance(i.internal, InternalListGroup):
+                    isentencelist(i.idtf, i.internal)
+                    
+                if item.attrs is not None:
+                    for attr in item.attrs:
+                        triples.append((attr, mk_arc("->") ,a_id))
+
+def iwigroup(item):
+    i = item.idtf
+    print i
+    if isinstance(i, TripleGroup):
+        pred = str(i.predicate)
+        pair = str(i)
+        if pair_idtfs.has_key(pair):
+            arc_id = pair_idtfs[pair]
+            arc = "%s/%s" % (arc_id, arc_types[pred])
+        else:
+            arc_id = mk_arc_id()
+            arc = "pair%d/%s" %(arc_id, arc_types[pred])
+            pair_idtfs[str(i)] = arc_id
+            triples.append((str(id.subject), arc, str(id.object)))                
+        return arc_id
+
+def presults(fields):
+    for f in fields:
+        processors[f.__class__](f)
+        
+def isentencelist(subj, sentList):
+    for s in sentList.sentences:
+        pred = str(s.predicate)
+        for obj in s.object:
+            if not isinstance(obj, IdtfWithIntGroup):
+                continue
+            
+            a_id = mk_arc_id()
+            arc = "%s/%s" %(a_id, arc_types[pred])
+            
+            if isinstance(obj.internal, InternalListGroup):
+                isentencelist(obj.idtf, obj.internal)
+            
+            
+            triples.append((str(subj), arc, str(obj.idtf)))
+            for attr in s.attrs:
+                triples.append((attr, mk_arc("->"), a_id))
+                
+def skip(item):
+    pass
+           
+processors = {SentenceGroup: sgroup,
+              IdtfWithIntGroup: iwigroup,
+              ParseResults: presults,
+              KeywordGroup: skip}
+
+os.chdir("./test")
+
+# process all sc.s files from the current directory and below
+# and write output
+for f in glob("*/*.scs") + glob("*.scs"):
+    presults(parse(f))
+    
+with codecs.open("out.scs1", "w", "utf-8") as output:
+    for t in triples:
+        output.write("%s|%s|%s;;\n" % t)
